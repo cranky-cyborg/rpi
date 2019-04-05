633eb3e49f90109b2b8c5784fc047d04d871bf5e
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-05 22:15:07 +1300

    removed bsd* as it was causing the first line to fail

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-sudo apt purge apt-utils manpages python* bsd* ssh* openssh* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
+sudo apt purge apt-utils manpages python* ssh* openssh* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
 sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
 sudo apt purge linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc -y
 sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* libc-l10n tzdata -y
@@ -8,7 +8,7 @@ sudo apt purge perl groff-base man-db geoid-database cpp cpp-6  -y
 
 #beloe are nice to have packages
 
-sudo apt purge aptitude* -y 
+#sudo apt purge aptitude* -y 
 
 # from below removed some questionable packages
-sudo apt purge libicu* -y
+#sudo apt purge libicu* -y
