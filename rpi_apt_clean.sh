ceaf781fc057d6ade7566c32e1be739286c8f585
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-05 16:20:50 +1300

    added few more steps to clear things better

--- rpi_apt_clean.sh
+++ rpi_apt_clean.sh
@@ -4,7 +4,21 @@ sudo apt-get clean -y
 sudo apt clean -y
 
 # delete unused kernel module folder
+sudo rm -rf /lib/modules/4.14.79-v7+/
+sudo rm -rf /var/cache/man/
 sudo rm -rf /usr/share/doc/
 sudo rm -rf /usr/share/man/
 sudo rm -rf /usr/share/locale/
 sudo rm -rf /etc/sgml/
+
+#removing swap file
+sudo swapoff /var/swap
+sudo rm /var/swap
+
+
+#must create a swap file on restoring the image.
+#sudo fallocate -l 250M /var/swap
+#sudo chmod 600 /var/swap
+#sudo dd if=/dev/zero of=/var/swap bs=1024 count=1048
+#sudo mkswap /var/swap
+#sudo swapon /var/swap
