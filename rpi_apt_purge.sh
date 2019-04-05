b8a9df73577998474b1cb1fedfeba461d8111c9b
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-06 10:58:17 +1300

    added a few more

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -1,6 +1,9 @@
 #/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#make sure you run the below (until reboot) before running this script
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+#sudo rpi-update
+#reboot
 
 sudo apt purge apt-utils manpages python* ssh* openssh* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
 sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
@@ -9,13 +12,11 @@ sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* tzdata -y
 
 sudo apt purge perl groff-base man-db cpp cpp-6  -y
 
-sudo apt purge geoip-database -y
+sudo apt purge geoip-database libnss-mdns luajit shared-mime-info libtext-charwidth-perl libtext-iconv-perl -y
 
 #beloe are nice to have packages
 
-#sudo apt purge aptitude* -y 
+sudo apt purge aptitude* -y 
 
 # from below removed some questionable packages
 #sudo apt purge libicu* -y
-
-sudo rpi-update .
