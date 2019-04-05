e2f609f4b585b54815489811360ddde5088d1897
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-05 16:21:00 +1300

    added a few more packeage to remove

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -3,3 +3,5 @@ sudo apt purge apt-utils manpages python* bsd* ssh* openssh* vim-tiny vim-common
 sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
 sudo apt purge linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc -y
 sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* libc-l10n tzdata -y
+
+sudp apt purge perl groff-base man-db geoid-database cpp cpp-6  -y
