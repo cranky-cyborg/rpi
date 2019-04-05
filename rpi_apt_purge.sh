b175d0a1463643123dd888ccca10cda3df2cf3f6
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-06 00:27:07 +1300

    moved geoip-db to new line

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -5,9 +5,11 @@ sudo apt-get upgrade -y
 sudo apt purge apt-utils manpages python* ssh* openssh* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
 sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
 sudo apt purge linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc -y
-sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* libc-l10n tzdata -y
+sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* tzdata -y
 
-sudo apt purge perl groff-base man-db geoid-database cpp cpp-6  -y
+sudo apt purge perl groff-base man-db cpp cpp-6  -y
+
+sudo apt purge geoip-database -y
 
 #beloe are nice to have packages
 
