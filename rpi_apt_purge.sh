624259eb2c3fdbc3b75852db1ddb62db53e32ead
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-07 18:23:07 +1200

    removed ssh from purging

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -5,7 +5,7 @@
 #sudo rpi-update
 #reboot
 
-sudo apt purge apt-utils manpages python* ssh* openssh* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
+sudo apt purge apt-utils manpages python* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
 sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
 sudo apt purge linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc -y
 sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* tzdata -y
