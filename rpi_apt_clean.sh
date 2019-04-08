7e146715b6998b27c57b9b82791819d0d3261d7e
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-08 12:13:36 +1200

    added a few more remove force stmts

--- rpi_apt_clean.sh
+++ rpi_apt_clean.sh
@@ -14,6 +14,14 @@ sudo rm -rf /lib/modules/4.14.79-v7+/
 sudo rm -rf /lib/modules/4.14.*
 sudo rm -rf /lib/modules/4.19.32+
 sudo rm -rf /boot.bak
+sudo rm -rf /var/lib/apt/lists
+sudo rm -r  /var/log/*
+sudo rm -r  /var/backups/*
+sudo rm -r  /var/tmp/*
+sudo rm -r  /var/caches/*
+sudo rm -r  /tmp/*
+sudo rm -r  /run/log/*
+sudo rm -rf /lib/modules.bak
 
 #removing swap file
 #sudo swapoff /var/swap
