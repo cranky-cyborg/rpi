59d9ad8c43bf99c989f1fc683b023db703b09a01
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2019-04-08 12:01:07 +1200

    Simplified into one line

--- rpi_apt_purge.sh
+++ rpi_apt_purge.sh
@@ -5,18 +5,13 @@
 #sudo rpi-update
 #reboot
 
-sudo apt purge apt-utils manpages python* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data -y
-sudo apt purge v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd debconf-utils -y
-sudo apt purge linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc -y
-sudo apt purge gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* tzdata xkb-data console-setup* triggerhappy sgml-base -y
+sudo apt purge apt-utils manpages python* vim-tiny vim-common ed xxd info install-info dc dos* cifs-utils libx11-data \
+               v4l-utils xml-core libpython3* debconf-i18n xz-utils bzip* unzip* tasksel rsyslog logrotate libpam-chksshpwd \
+               debconf-utils linux-libc-dev libmnl-dev* libraspberrypi-dev* libraspberrypi-doc \
+               gcc-4.6* gcc-4.7* gcc-4.8* gcc-4.9* gcc-5* tzdata xkb-data console-setup* triggerhappy sgml-base \
+               perl groff-base man-db cpp cpp-6 geoip-database libnss-mdns luajit shared-mime-info libtext-charwidth-perl \
+               libtext-iconv-perl aptitude* -y
 
-sudo apt purge perl groff-base man-db cpp cpp-6  -y
-
-sudo apt purge geoip-database libnss-mdns luajit shared-mime-info libtext-charwidth-perl libtext-iconv-perl -y
-
-#beloe are nice to have packages
-
-sudo apt purge aptitude* -y 
 
 # from below removed some questionable packages
 #sudo apt purge libicu* -y
