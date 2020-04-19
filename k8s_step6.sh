9b085d4952e8c5c806d86f8663535acb2ee07c31
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2020-04-19 21:07:33 +1200

    Update k8s_step6.sh

--- k8s_step6.sh
+++ k8s_step6.sh
@@ -3,5 +3,5 @@
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
