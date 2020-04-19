de6184a3eecad1398c04d25607c53e4af92a72ef
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2020-04-19 20:31:54 +1200

    Create k8s_step3.sh

---
+++ k8s_step3.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+sudo modprobe br_netfilter
+
+sudo cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
