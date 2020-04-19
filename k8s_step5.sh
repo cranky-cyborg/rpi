6f35fb6d8b915773b51baba3cd98b687bb088061
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2020-04-19 20:37:29 +1200

    Update k8s_step5.sh

--- k8s_step5.sh
+++ k8s_step5.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-sudo cp /boot/cmdline.txt /boot/cmdline_bkup.txt
+cp /boot/cmdline.txt /boot/cmdline_bkup.txt
 
-sudo orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
 
 echo $orig | tee /boot/cmdline.txt
 
