fffd8259d76cad0ccf78692364c8c64603b68e5f
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2020-04-19 21:46:24 +1200

    Update k8s_setup.sh

--- k8s_setup.sh
+++ k8s_setup.sh
@@ -22,19 +22,19 @@
 # - enable ssh interface
 # - Expand filesystem
 # - Memory Split to 32
-
+echo " "
 echo "Below you will be prompted to reset your password."
 echo " RaspberryPi default password is 'raspberry'."
 passwd -q
-
+echo " "
 echo "Hope that went well!, Now lets change the host name."
 echo "As this machine will become a node (or master) of kubernetes"
 echo "it is recommeded that you follow a pattern, something like 'node-001'"
 echo "This will ensure, your machine naming is consistent and easy to identify".
-echo ""
-read "Now, what do you want to name this machine: " machinenane
+echo " "
+read "Now, what do you want to name this machine: " newHostName
 
-hostnamectl set-hostname $machinename
+hostnamectl set-hostname $newHostName
 
 hostnamectl
 
